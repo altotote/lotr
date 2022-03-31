2ce0b083eb19bb88928da41c9335fbf51bddd0b3
altotote <45882858+altotote@users.noreply.github.com>
2022-03-30 23:33:51 -0600

    Agregar funcion para selector

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const head = {
     "Authorization": "Bearer DZPD3yrWo3FW9aAqH-dt"
 }
 
-const url = "https://the-one-api.dev/v2/quote"
-
 // Funcion para tener nueva quote 
 const getquote = () => {
     let num = Math.floor(Math.random()*1000 );
     console.log(num)
+    let url = "https://the-one-api.dev/v2/quote"
     fetch(url, {
         headers: head
     })
@@ -32,6 +31,31 @@ const getquote = () => {
     })
 }
 
+// Funcion para obtener quotes de un personaje en particular 
+const searchChar = () => {
+    const character = document.getElementById("select");
+    console.log(character.value)
+    let url = `https://the-one-api.dev/v2/quote?character=${character.value}`
+    fetch(url, {
+        headers: head
+    })
+    .then(res => res.json())
+    .then(data => {
+        let num = Math.floor(Math.random()*data.docs.length);
+        console.log(data.docs.length)
+        var newQute = data.docs[num];
+        const url2 = `https://the-one-api.dev/v2/character?_id=${newQute.character}`
+        fetch(url2, {
+            headers: head
+        })
+        .then(res => res.json())
+        .then(data =>{
+            setQuote(newQute.dialog, data.docs[0].name);
+            
+        })
+
+    })
+}
 // Funcione de escritura para desplegar la informacion
 const setQuote = (dialog, author) => {
     const quote = document.getElementById("quote");
